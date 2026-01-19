1a2031172886e287fc4ee28700e7e0bc9350e323
aicha21000 <salhi.aichal@hotmail.com>
2026-01-19 17:47:38 +0100

    facebook button

--- src/app/products/[id]/ProductDetailClient.tsx
+++ src/app/products/[id]/ProductDetailClient.tsx
@@ -136,6 +136,29 @@ export default function ProductDetailClient({ product }: ProductDetailClientProp
                             </>
                         )}
                     </button>
+
+                    <button
+                        className={styles.facebookBtn}
+                        onClick={() => {
+                            const url = window.location.href;
+                            window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`, '_blank');
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        >
+                            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+                        </svg>
+                        Partager sur Facebook
+                    </button>
                 </div>
             </div>
         </div>
